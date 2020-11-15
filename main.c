54bbca1fe0954ade2d9fc7e4e3fbc21c2a1fdfb3
slayer404 <doomer888@protonmail.com>
2020-11-15 18:51:13 +0300

    added a raw mode

--- main.c
+++ main.c
@@ -1,5 +1,31 @@
 #include <stdio.h> 
+#include <unistd.h>
+#include <termios.h>
+#include <stdlib.h>
+
+struct termios canonicalMode; 
+
+void readKeyboard() {
+    char c; 
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q'); 
+}
+
+void disableRawMode() {
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &canonicalMode); 
+}
+
+void enableRawMode() {
+    tcgetattr(STDIN_FILENO, &canonicalMode); 
+    atexit(disableRawMode);
+
+    struct termios rawMode = canonicalMode; 
+    rawMode.c_lflag &= (~ECHO | ICANON); 
+
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &rawMode); 
+}
+
 int main() {
-    printf("Hello\n");
+    enableRawMode(); 
+    readKeyboard(); 
     return 0;
 }
